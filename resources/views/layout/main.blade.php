b4f2ea47b251efe8c8c0ae62be56c9738e494f7b
unknown <sofyanmuhammad071@gmail.com>
2024-07-15 11:59:34 +0700

    feat : layout main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title')</title>
     <link rel="shortcut icon" type="image/png" href="{{ asset('/logo.png') }}" />
     <link rel="stylesheet" href="{{ asset('/css/styles.min.css') }}" />
@@ -20,7 +21,7 @@
             <!-- Sidebar scroll-->
             <div>
                 <div class="brand-logo d-flex align-items-center justify-content-between">
-                    <a 
+                    <a
                         href=""
                         class="text-nowrap logo-img"
                         style="padding-top: 1.5rem"
@@ -46,6 +47,7 @@
                                 <span class="hide-menu">Dashboard</span>
                             </a>
                         </li>
+                        @if(auth()->user()->hasRole('base.role_superadmin'))
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="/banner" aria-expanded="false">
                                 <span>
@@ -54,6 +56,8 @@
                                 <span class="hide-menu">Banner</span>
                             </a>
                         </li>
+                        @endif
+                        @if(auth()->user()->hasRole('base.role_admin'))
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="/product" aria-expanded="false">
                                 <span>
@@ -62,6 +66,8 @@
                                 <span class="hide-menu">Product</span>
                             </a>
                         </li>
+                        @endif
+                        @if(auth()->user()->hasRole('base.role_admin'))
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="/transaction" aria-expanded="false">
                                 <span>
@@ -70,6 +76,8 @@
                                 <span class="hide-menu">Transaksi</span>
                             </a>
                         </li>
+                        @endif
+                        @if(auth()->user()->hasRole('base.role_superadmin'))
                         <li class="sidebar-item">
                             <a class="sidebar-link" href="/users" aria-expanded="false">
                                 <span>
@@ -78,6 +86,7 @@
                                 <span class="hide-menu">Users</span>
                             </a>
                         </li>
+                        @endif
                     </ul>
                 </nav>
                 <!-- End Sidebar navigation -->
@@ -97,37 +106,30 @@
                                 <i class="ti ti-menu-2"></i>
                             </a>
                         </li>
-                        
+
                     </ul>
                     <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
                         <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
                             <li class="nav-item dropdown">
                                 <a class="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2"
                                     data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="{{ asset('/images/profile/user-1.jpg') }}" alt="" width="35"
+                                    <img src="{{ asset(auth()->user()->profile??'images/profile/no-images.png') }}" alt="" width="35"
                                         height="35" class="rounded-circle">
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
                                     aria-labelledby="drop2">
                                     <div class="message-body">
-                                        <a href="javascript:void(0)"
-                                            class="d-flex align-items-center gap-2 dropdown-item">
-                                            <i class="ti ti-user fs-6"></i>
-                                            <p class="mb-0 fs-3">My Profile</p>
-                                        </a>
-                                        <a href="javascript:void(0)"
-                                            class="d-flex align-items-center gap-2 dropdown-item">
+                                        <button
+                                            class="d-flex align-items-center gap-2 dropdown-item"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#modalProfileEdit{{auth()->user()->id}}"
+                                        >
                                             <i class="ti ti-mail fs-6"></i>
                                             <p class="mb-0 fs-3">My Account</p>
-                                        </a>
-                                        <a href="javascript:void(0)"
-                                            class="d-flex align-items-center gap-2 dropdown-item">
-                                            <i class="ti ti-list-check fs-6"></i>
-                                            <p class="mb-0 fs-3">My Task</p>
-                                        </a>
+                                        </button>
                                         <form action="{{ route('logout') }}" method="POST">
                                         @csrf
-                                        <button 
+                                        <button
                                             type="submit"
                                             class="btn btn-outline-primary mx-3 mt-2 d-block">Logout
                                         </button>
@@ -141,6 +143,59 @@
             </header>
             <!--  Header End -->
             @yield('main')
+
+            <!-- Profile Change Modal -->
+            <div class="modal fade" id="modalProfileEdit{{auth()->user()->id}}" tabindex="-1" role="dialog" aria-labelledby="modalProfileEdit{{auth()->user()->id}}Title" aria-hidden="true">
+                <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <form enctype="multipart/form-data" method="POST" action="{{ route('users.edit', ['id' => auth()->user()->id]) }}">
+                        @csrf
+                        @method("PUT")
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalProfileEdit{{auth()->user()->id}}Title">My Profile</h5>
+                            <button type="button" class="close btn btn-error" data-bs-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row mb-3">
+                                <div class="mb-3 col-sm-12 col-md-6">
+                                    <label for="name" class="form-label">Name</label>
+                                    <input type="text" value="{{ auth()->user()->name }}" name="name" class="form-control" id="name" aria-describedby="name">
+                                </div>
+                                <div class="mb-3 col-sm-12 col-md-6">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" value="{{ auth()->user()->email }}" name="email" class="form-control" id="email" aria-describedby="email">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="mb-3 col-sm-12 col-md-6">
+                                    <label for="description" class="form-label">Description</label>
+                                    <input type="text" value="{{ auth()->user()->description }}" name="description" class="form-control" id="description" aria-describedby="description">
+                                </div>
+                                <div class="mb-3 col-sm-12 col-md-6">
+                                    <label for="address" class="form-label">Address</label>
+                                    <input type="text" value="{{ auth()->user()->address }}" name="address" class="form-control" id="address" aria-describedby="address">
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" name="password" class="form-control" id="password" aria-describedby="password">
+                            </div>
+                            <div class="mb-3">
+                                <label for="profile" class="form-label">Profile</label>
+                                <input type="file" name="profile" class="form-control" id="profile" aria-describedby="profile">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- end -->
         </div>
     </div>
     <script src="{{ asset('/libs/jquery/dist/jquery.min.js') }}"></script>
